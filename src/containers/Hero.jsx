4ce873a5e80e18ca748912b9dd85468d76a40e1f
ebukathedev <ebuskelele@gmail.com>
2023-10-01 18:06:52 +0100

    update download link

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -4,7 +4,6 @@ import heroImg3 from "../images/hero-img_3.png";
 import arrowRight from "../images/arrow-right.svg";
 import dottedSquare1 from "../images/dotted-square_1.svg";
 import dottedSquare2 from "../images/dotted-square_2.svg";
-import chromeExtensionZip from "../../helpmeout-chrome-extension.zip";
 
 const Hero = () => {
 	return (
@@ -19,9 +18,9 @@ const Hero = () => {
 						videos on how to get things done on a website.
 					</p>
 					<a
-						href={chromeExtensionZip}
+						href="/helpmeout-chrome-extension.zip"
 						className="flex items-center w-fit text-white px-6 py-3 font-workSans font-medium bg-primary space-x-[10px] rounded-lg text-base mt-8 hover:space-x-4 lg:px-6 lg:py-5 lg:text-lg lg:mt-12"
-						download
+						download="helpmeout-chrome-extension.zip"
 					>
 						<span>Install HelpMeOut</span>
 						<div className="transition-all">
